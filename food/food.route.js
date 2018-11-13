9c0e1039a87519412389d8b12487bde36ac628d4
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-11-13 14:51:01 +1100

    Added a /food/:id PUT handler

--- food/food.route.js
+++ food/food.route.js
@@ -51,11 +51,43 @@ router.post('/', (req, res, next) => {
  */
 router.get('/:id', (req, res, next) => {
     Food.findOne({_id: req.params.id})
-        .then(record => record ?
-            res.wrap(record.exportable) :
-            next() // the the 404 handler catch it
-        )
+        .catch(() => next()) // let the 404 handler catch it
+        .then(record => res.wrap(record.exportable))
         .catch(err => next({status: 500, message: 'Server Error: Unable to retrieve the specified Food record'}));
+});
+
+/**
+ * Update the specified food record
+ */
+router.put('/:id', (req, res, next) => {
+    const update = {};
+
+    if (req.body.name) {
+        if (!req.body.name.singular || typeof req.body.name.singular !== 'string')
+            return next({status: 400, message: 'If name is updated, name.singular must be a string'});
+        if (!req.body.name.plural || typeof req.body.name.plural !== 'string')
+            return next({status: 400, message: 'If name is updated, name.plural must be a string'});
+        update.name = req.body.name;
+    }
+    if (req.body.conversions) {
+        if (!Array.isArray(req.body.conversions))
+            return next({status: 400, message: 'If conversions is updated, it must be an array'});
+        req.body.conversions.forEach(c => {
+            if (c.unit_id === null || typeof c.unit_id !== 'number' || !Number.isInteger(c.unit_id)
+                || c.unit_id < 0 || c.unit_id > 12)
+                return next({status: 400, message: 'Each conversion.unit_id must be an integer between 0 and 12'});
+            if (c.ratio === null | typeof c.ratio !== 'number')
+                return next({status: 400, message: 'Each conversion.ration must be a number'});
+        });
+        update.conversions = req.body.conversions;
+    }
+
+    Food.findOne({_id: req.params.id})
+        .catch(() => next()) // let the 404 handler catch it
+        .then(record => Object.assign(record, update))
+        .then(record => record.save())
+        .then(() => res.status(204).send())
+        .catch(err => next({status: 500, message: 'Server Error: Unable to update the specified Food record'}));
 });
 
 module.exports = router;
