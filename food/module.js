661bc142e1de85f2959a037bd5d1d34900c95166
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-11-28 14:47:49 +1100

    Added the food samples to the Food module

--- food/module.js
+++ food/module.js
@@ -3,9 +3,11 @@
 const route = require('./food.route');
 const model = require('./food.model');
 const unit_types = require('./unit_types');
+const sample = require('./food.sample');
 
 module.exports = {
     route: route,
     model: model,
-    unit_types: unit_types
+    unit_types: unit_types,
+    sample: sample
 };
