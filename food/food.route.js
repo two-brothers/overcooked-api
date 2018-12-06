20eda65611e7e8055919097a38b9531eb522ee44
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-06 21:16:35 +1100

    Sort food items by ID in food/at/:page handler
    The handler is supposed to return the specified page of food.
    The order of the items is not important, but the same order should
    be used every time to ensure the items are never shared between pages
    (unless the database changes)
    
    This commit sorts and selects the items based on the food ID

--- food/food.route.js
+++ food/food.route.js
@@ -126,6 +126,7 @@ router.get('/at/:page', (req, res, next) => {
         next({status: 400, message: error});
 
     Food.find()
+        .sort({id: 1})
         .limit(ITEMS_PER_PAGE + 1) // get an extra record to see if there is at least another page,
         .skip(page * ITEMS_PER_PAGE)
         .then(records => records.length === 0 ? Promise.reject(RecordsNotFound) : records) // let the 404 handler catch it
